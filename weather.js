50116210f9c304eb89425afb644fec22ff2fcd98
Greg Mortner <greg.mortner@kellogg.northwestern.edu>
2017-10-20 13:26:47 -0500

    First Attempt
    Added location + update to html code. Need to get API to correctly pull

--- weather.js
+++ weather.js
@@ -1,14 +1,37 @@
 
-let getWeather = function() {
-  let latitude = '41.8781';
-  let longitude = '-87.6298';
+let getWeather = function(info) {
+  console.debug("Your device's current longitude is: ");
+  console.debug(info.coords.longitude.toFixed(4));
+  console.debug("Your device's current latitude is: ");
+  console.debug(info.coords.latitude.toFixed(4));
+
+  let lat = info.coords.latitude.toFixed(4); //'41.8781'
+  let lon = info.coords.longitude.toFixed(4); //'-87.6298'
   let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
-  openweathermap_api_url += 'lat=' + latitude
-  openweathermap_api_url += '&lon=' + longitude
+  openweathermap_api_url += 'lat=' + lat
+  openweathermap_api_url += '&lon=' + lon
   openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
 
   fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
+
+
+  let updatePic = document.getElementById("weather_pic");
+  updatePic.innerHTML = "http://openweathermap.org/img/w/" + openweathermap_api_url.weather.icon + ".png";
+
+  let update_Location = document.getElementById("loc");
+  updateTemp.innerHTML = openweathermap_api_url.name;
+
+  let updateTemp = document.getElementById("temp");
+  updateTemp.innerHTML = "It is "+ openweathermap_api_url.main.temp +" degrees outside";
 }
+
+//Get computer location
+let link = document.getElementById("get_forecast")
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getWeather);
+})
+
 
 let displayError = function(error) {
   console.debug(error);
